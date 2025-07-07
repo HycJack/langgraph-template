21f215520bd3feea8d82a43345fae49d27038f29
HycJack <772403255@qq.com>
2025-07-07 11:47:26 +0800

    Update visual_graph.py

--- utils/visual_graph.py
+++ utils/visual_graph.py
@@ -1,7 +1,19 @@
 from IPython.display import Image, display
-from ..agent import graph
+from agent import build_graph
 try:
+    graph = build_graph()
     display(Image(graph.get_graph().draw_mermaid_png()))
 except Exception:
+    # This requires some extra dependencies and is optional
+    pass
+
+try:
+    graph = build_graph()
+    png_data = graph.get_graph().draw_mermaid_png()
+    with open("graph.png", "wb") as f:
+        f.write(png_data)
+    display(Image(png_data))
+except Exception as e:
+    print(f"Error generating or saving graph: {e}")
     # This requires some extra dependencies and is optional
     pass
